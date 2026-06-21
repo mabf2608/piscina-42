8c36517f7d84580d0ae73babd6a0cc914259a686
mabf2608 <m.a.boni26@hotmail.com>
2026-06-21 11:29:59 +0200

    Fin del Rush00

--- ejercicios/Rush00/rush_04/rush_04.c
+++ ejercicios/Rush00/rush_04/rush_04.c
@@ -6,7 +6,7 @@
 /*   By: mabf2608 <mabf2608@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/20 11:00:24 by mibonill          #+#    #+#             */
-/*   Updated: 2026/06/20 16:56:34 by mibonill         ###   ########.fr       */
+/*   Updated: 2026/06/21 11:25:19 by mibonill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,23 @@ void	rush(int x, int y)
 	int	i;
 	int	j;
 
-	i = 1;
-	j = 1;
-	while (i <= y)
+	if (x < 1 || y < 1)
 	{
-		j = 1;
-		while (j <= x)
+		write(1, "Error: usa solo números > 1.\n", 30);
+	}
+	else
+	{
+		i = 1;
+		while (i <= y)
 		{
-			ft_putchar(i, j, y, x);
-			j++;
+			j = 1;
+			while (j <= x)
+			{
+				ft_putchar(i, j, y, x);
+				j++;
+			}
+			write (1, "\n", 1);
+			i++;
 		}
-		write (1, "\n", 1);
-		i++;
 	}
 }
