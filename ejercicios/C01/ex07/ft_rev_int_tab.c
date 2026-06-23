1da4cec7f8854b4c1ac901a9a04f171e435fa438
mabf2608 <m.a.boni26@hotmail.com>
2026-06-23 09:44:08 +0200

    Intento ex07 de C01

--- ejercicios/C01/ex07/ft_rev_int_tab.c
+++ ejercicios/C01/ex07/ft_rev_int_tab.c
@@ -6,23 +6,29 @@
 /*   By: mibonill <mibonill@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/19 15:10:56 by mibonill          #+#    #+#             */
-/*   Updated: 2026/06/19 15:23:45 by mibonill         ###   ########.fr       */
+/*   Updated: 2026/06/22 20:35:02 by mibonill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include <unistd.h>
 
 void	ft_rev_int_tab(int *tab, int size)
-{
-	char	i;
-	int	arr[size];
+{	
+	char	aux;
+	int	n [size];
 
-	
-	i = 0;
-	while (i >= size)
+
+	while (n >= tab)
 	{
-		arr[i] = tab
-		
-		i++;
+		aux = *n + '0'; 
+		write (1, &aux, 1);
+		n--;
 	}
+}
+
+int	main(void)
+{
+	int tab[] = {4321};
+	ft_rev_int_tab(tab, 4);
+	return (0);
 }
