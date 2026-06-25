6eaffc54ed654dd9fd05133ae21616ff871f7c25
mabf2608 <m.a.boni26@hotmail.com>
2026-06-25 14:35:50 +0200

    Ejercicio ex10 C02

--- ejercicios/C02/ex10/ft_strlcpy.c
+++ ejercicios/C02/ex10/ft_strlcpy.c
@@ -3,13 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mibonill <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mibonill <mibonill@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/06/25 13:38:07 by mibonill          #+#    #+#             */
-/*   Updated: 2026/06/25 13:47:54 by mibonill         ###   ########.fr       */
+/*   Created: 2026/06/25 14:16:16 by mibonill          #+#    #+#             */
+/*   Updated: 2026/06/25 14:34:31 by mibonill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 
-unsigned int
+unsigned int ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned	int		i;
+
+	i = 0;
+	while (*src)
+	{
+		if (i <= size - 1)
+		{
+			*dest = *src;
+			write (1, dest, 1);
+		}
+		dest++;
+		src++;
+		i++;
+	}
+	*dest = '\0';
+	return (i);
+}
+
+int		main(void)
+{
+	int		x = 5;
+	char	*str = "Hola Buenas";
+	char	dest[x];
+	int aux = ft_strlcpy(dest, str, x);
+	printf("%d", aux);
+
+	return (0);
+}
