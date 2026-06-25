f18e3ac7d2dc064aca38df6b3d9e9c905a7432d2
mabf2608 <m.a.boni26@hotmail.com>
2026-06-25 13:48:28 +0200

    Ejercicio ex09 resuelto e inicio ex10 de C02

--- ejercicios/C02/ex09/ft_strcapitalize.c
+++ ejercicios/C02/ex09/ft_strcapitalize.c
@@ -6,7 +6,7 @@
 /*   By: mibonill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/25 09:24:45 by mibonill          #+#    #+#             */
-/*   Updated: 2026/06/25 12:24:17 by mibonill         ###   ########.fr       */
+/*   Updated: 2026/06/25 13:35:05 by mibonill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,45 +14,35 @@
 
 char	*ft_strcapitalize(char *str)
 {
+	int		bool;
 	char	*temp;
-	int	bool;
-	
+
 	bool = 1;
 	temp = str;
 	while (*str)
 	{
+		if (*str >= 'A' && *str <= 'Z')
+			*str = *str + 32;
 		if (*str >= 'a' && *str <= 'z' && bool)
-		{
-			bool = 0;
 			*str = *str - 32;
-		}
 		if (*str >= '0' && *str <= '9')
-		{
 			bool = 0;
-		}
-		else if (*str >= 'A' && *str <= 'Z')
-		{
-			if(!bool)
-				*str = *str + 32;
-			bool = 0;
-		}
 		else if (*str >= 'a' && *str <= 'z')
-		{
 			bool = 0;
-		}
+		else if (*str >= 'A' && *str <= 'Z')
+			bool = 0;
 		else
-		{
 			bool = 1;
-		}
 		str++;
 	}
 	return (temp);
 }
-
-int	main (void)
+/*
+int	main(void)
 {
-	char	str[] = "salut, COMMENT tu Vas ? 42mots QUARANTE-DEUX; cinquante+et+un";
+	char	str[] = "salut, COMMENT tu+Vas ? 42mots";
 	char	*result = ft_strcapitalize(str);
-	write (1, result, 61);
+	write (1, result, 30);
 	return (0);
 }
+*/
