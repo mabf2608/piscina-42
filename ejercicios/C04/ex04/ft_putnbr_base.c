106a83e096ebb638e7261af4d2a7d077aa44eba1
mabf2608 <m.a.boni26@hotmail.com>
2026-06-30 13:11:23 +0200

    Avance ex04 C04

--- ejercicios/C04/ex04/ft_putnbr_base.c
+++ ejercicios/C04/ex04/ft_putnbr_base.c
@@ -6,11 +6,46 @@
 /*   By: mibonill <mibonill@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/30 12:21:07 by mibonill          #+#    #+#             */
-/*   Updated: 2026/06/30 12:23:48 by mibonill         ###   ########.fr       */
+/*   Updated: 2026/06/30 12:57:56 by mibonill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_putnbr_base(int nbr, char *base);
+char	*ft_to_10(long num_l);
+char	*ft_to_binary(long num_l)
+{
+	char	str[10];
+	int		i;
+
+	i = 0;
+	while (num_l > 0)
+	{
+		str[i] = (num_l % 2) + '0';
+		num_l = num_l / 10;
+	}
+	return (str);
+}
+char	*ft_to_hex(int nbr);
+char	*ft_to_octal(int nbr);
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	char	*arr_b[4];
+	long	num_l;
+	int		is_neg;
+	
+	is_neg = 0;
+	num_l = nbr;
+	if (num_l < 0)
+	{	
+		num_l = num_l * -1; 
+		is_neg = 1;
+	}
+	arr_b[0] = "0123456789";
+	arr_b[1] = "01";
+	arr_b[2] = "0123456789ABCDEF";
+	arr_b[3] = "poneyguay";
+
+}
 
 //Usa un array con las diferentes bases
 //base 10, binario, hexadecimal, octal
