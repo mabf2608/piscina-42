1b10377ae9976623f7ddc33c2663a099b36c997e
mabf2608 <m.a.boni26@hotmail.com>
2026-06-30 12:34:20 +0200

    Ejercicio ex03 del C04 completo e inicio del ex04

--- ejercicios/C04/ex03/ft_atoi.c
+++ ejercicios/C04/ex03/ft_atoi.c
@@ -6,20 +6,21 @@
 /*   By: mibonill <mibonill@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/29 20:32:36 by mibonill          #+#    #+#             */
-/*   Updated: 2026/06/29 21:42:22 by mibonill         ###   ########.fr       */
+/*   Updated: 2026/06/30 12:17:49 by mibonill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+//#include <unistd.h>
+//#include <stdio.h>
 
-int		ft_pos_or_neg(char *str)
+int	ft_pos_or_neg(char *str)
 {
 	int	i;
 	int	pair;
-	
-	pair = 0; 
+
+	pair = 0;
 	i = 0;
-	while (str[i] < '0' && str[i] > '9')
+	while (str[i] <= '0' || str[i] >= '9')
 	{
 		if (str[i] == '-')
 			pair++;
@@ -33,47 +34,50 @@ int		ft_pos_or_neg(char *str)
 
 char	*ft_no_prefix(char *str)
 {
-	while (*str < '0' && *str > '9')
+	while (*str < '0' || *str > '9')
 		str++;
 	return (str);
 }
 
-int		ft_get_nbr(char *str)
+int	ft_get_nbr(char *str)
 {
 	int	i;
+	int	j;
+	int	aux;
 
 	i = 0;
-	while(str[i] != '\0')
+	while (str[i] >= '0' && str[i] <= '9')
 		i++;
 	i = i - 1;
-
-	int	temp[i];
-	int aux;
-	
+	j = 0;
 	aux = 0;
-	while( i >= 0)
+	while (i >= 0)
 	{
-		aux = aux(*10) +((str[i] - '0'));
-	   	i--;
+		aux = (aux * 10) + ((str[j] - '0'));
+		i--;
+		j++;
 	}
 	return (aux);
 }
 
-int		ft_atoi(char *str);
+int	ft_atoi(char *str)
 {
-	//Primero filtrar los espacios iniciales y quitarlos de la string
-	//Filtrar los signos '-' si cantidad % 2 = 0 par si no 1 y quitarlos del array 
-	//(luego se multiplica según el v		alor * -1
-	//Procesar cualquier cantidad de digitos en base 10 y lo devuelve hasta donde deja de ser numero
-	//No controlar overflow ni underflow (se puede hacer con long) ya que queda indefinido
 	int	pair;
 	int	nbr;
 
 	pair = ft_pos_or_neg(str);
 	str = ft_no_prefix(str);
 	nbr = ft_get_nbr(str);
-
 	if (pair == 0)
 		nbr = nbr * -1;
 	return (nbr);
 }
+/*
+int	main(void)
+{
+	int aux;
+	aux = ft_atoi(" ---+---asf634ab567");
+	printf("%d", aux);
+	return (0);
+}
+*/
---
+++ ejercicios/C04/ex04/ft_putnbr_base.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mibonill <mibonill@student.42malaga.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/30 12:21:07 by mibonill          #+#    #+#             */
+/*   Updated: 2026/06/30 12:23:48 by mibonill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putnbr_base(int nbr, char *base);
+
+//Usa un array con las diferentes bases
+//base 10, binario, hexadecimal, octal
+//controlar numeros negativos
+//si se da parametro invalido controlar errores
