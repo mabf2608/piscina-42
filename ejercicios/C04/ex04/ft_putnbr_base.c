d590297fc97eb3766613c08043eff4778467763d
mabf2608 <m.a.boni26@hotmail.com>
2026-07-02 13:35:55 +0200

    Correción ex10 del C02 y Modificaciones ex04 C04

--- ejercicios/C04/ex04/ft_putnbr_base.c
+++ ejercicios/C04/ex04/ft_putnbr_base.c
@@ -6,65 +6,70 @@
 /*   By: mibonill <mibonill@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/30 12:21:07 by mibonill          #+#    #+#             */
-/*   Updated: 2026/06/30 20:22:31 by mibonill         ###   ########.fr       */
+/*   Updated: 2026/07/02 12:13:52 by mibonill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-
-char	*ft_to_10(long num_l);
-char	*ft_to_binary(long num_l, char **temp)
+#include <stdio.h>
+void	ft_to_10(long num_l)
 {
-	int		i;
-	int		aux;
+	char	temp[10];
+	int		nb;
 
-	i = 32;
+	nb = 0;
+	if (num_l == 0)
+		write (1, "0", 1);
+	else
+	{
+		if (num_l < 0)
+		{
+			num_l = num_l * -1;
+			write (1, "-", 1);
+		}
+		while (num_l != 0)
+		{
+			temp[nb] = (num_l % 10) + '0';
+			num_l = num_l / 10;
+			nb++;
+		}
+		nb = nb -1;
+	}
+	while (nb >= 0)
+	{
+		write(1, &temp[nb], 1);
+		nb--;
+	}
+}
+
+void	ft_to_binary(long num_l)
+{
+	char	aux[32];
+	int		i;
+
+	i=0;
 	while (num_l > 0)
 	{
-		aux = (num_l % 2) + '0';
-		temp[i] = aux + '0';
-		num_l = num_l / 10;
+		aux[i] = (num_l % 2) + '0';
+		num_l = num_l / 2;
+		i++;
+	}
+
+	i--;
+	while (i>=0)
+	{
+		write(1, &aux[i], 1);
 		i--;
 	}
-	return (temp);
 }
-char	*ft_to_hex(int nbr);
-char	*ft_to_octal(int nbr);
+//char	*ft_to_hex(int nbr);
+//char	*ft_to_octal(int nbr);
 
-void	ft_putnbr_base(int nbr, char *base)
-{	
-	char	*temp[32];
-	char	*arr_b[4];
-	long	num_l;
-	//int		is_neg;	
-	
-	//is_neg = 0;
-	num_l = nbr;
-	nbr = 0;
-	if (num_l < 0)
-	{	
-		num_l = num_l * -1; 
-		//is_neg = 1;
-	}
-	arr_b[0] = "0123456789";
-	arr_b[1] = "01";
-	arr_b[2] = "0123456789ABCDEF";
-	arr_b[3] = "poneyguay";
+//void	ft_putnbr_base(int nbr, char *base);
 
-	if (arr_b[1] == base)
-		temp = (ft_to_binary(num_l, temp));
-
-	while (temp[nbr] != '\0')
-	{
-		write (1,temp[nbr], 1);
-		nbr++;
-	}
-}
-
-int	main(int argc, char **argv)
+int	main(void)
 {
-	if (argc > 1 && argv[1][0] != '\0')
-		ft_putnbr_base(argv[1][0], "01");
+    ft_to_10(1234566);
 	return (0);
 }
 
